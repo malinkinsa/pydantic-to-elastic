fa1f6ddfaba4a49e490132c976ba286fbe47e587
Sergey Malinkin <malinkinsa@yandex.ru>
2025-05-02 20:29:11 +0300

    bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
 
 setup(
     name='pydantic-to-elastic',
-    version='0.0.2',
+    version='0.0.3',
     description='A simple CLI utility for converting Pydantic models to Elasticsearch mappings',
     license='MIT',
     long_description=readme(),
